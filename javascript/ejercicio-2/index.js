a026717493a087588a1341e73d2707b8f3bb07ae
Luis Chilo <gfxargentina@gmail.com>
2022-06-07 15:30:57 -0300

    ejercicio 2 array objetos fechas

---
+++ javascript/ejercicio-2/index.js
@@ -0,0 +1,11 @@
+const fechaNacimiento = new Date(1999, 01, 12);
+const libroFavorito = {
+  titulo: "21 lecciones para el siglo XXI",
+  autor: "Yuval Noah Harari",
+  fecha: new Date(2018, 08, 01),
+  url: "https://es.wikipedia.org/wiki/21_lecciones_para_el_siglo_XXI",
+};
+
+const misDatos = ["agustin", "31", true, fechaNacimiento, libroFavorito];
+
+console.log(misDatos);
